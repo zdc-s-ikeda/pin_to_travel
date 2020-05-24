14ab99d8e32780b748fa8de686d891172bc9efdf
EC2 Default User <ec2-user@ip-172-31-80-13.ec2.internal>
2020-05-24 22:41:09 +0900

    const.phpのpasswdを自分のものに変更

--- conf/const.php
+++ conf/const.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'codecamp34610');
-define('DB_PASSWD', 'codecamp34610');
-define('DB_NAME', 'codecamp34610');
+define('DB_USER', 'codecamp34624');
+define('DB_PASSWD', 'codecamp34624');
+define('DB_NAME', 'codecamp34624');
 
 define('HTML_CHARACTER_SET', 'UTF-8');
 define('DB_CHARACTER_SET', 'UTF8');
