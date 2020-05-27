8bf6b835fd905bec5df3b6fa0941366249123a0e
EC2 Default User <ec2-user@ip-172-31-46-52.ec2.internal>
2020-05-27 14:40:01 +0900

    #25.2 ユーザーページ投稿内容表示

--- sign_up/conf/sign_up_const.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_HOST',   'localhost'); // データベースのホスト名又はIPアドレス
-define('DB_USER',   getenv('DB_USER'));  // MySQLのユーザ名
-define('DB_PASSWD', getenv('DB_USER'));    // MySQLのパスワード
-define('DB_NAME',   getenv('DB_USER'));    // データベース名
-define('HTML_CHARACTER_SET', 'UTF-8');  // HTML文字エンコーディング
-define('DB_CHARACTER_SET',   'UTF8');   // DB文字エンコーディング
