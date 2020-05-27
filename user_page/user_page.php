c03ec69b7a51a20cd79631c5984a5a3434f0a0b1
EC2 Default User <ec2-user@ip-172-31-42-107.ec2.internal>
2020-05-27 16:33:07 +0900

    代理で結合

--- user_page/user_page.php
+++ user_page/user_page.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../model/function_user_page.php';
+require_once '../model/user_page_func.php';
 require_once '../conf/const.php';
 
 
@@ -19,7 +19,21 @@ $list_items = get_list($link);
 $list_items_json = json_encode($list_items, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
 //var_dump($list_items);
 
-//db切断
+//ユーザー情報取得
+$user_info = get_user_info($link, $user_id);
+//これまでポストしたプレイスデータ取得
+$places = get_post_place($link, $user_id);
+//ルート情報の取得
+$route_list = get_route_info($link, $user_id);
+//ポストで選択されたルートID
+if (is_post() === TRUE) {
+    $route_id = get_post_data('route_id');
+    dd($_POST);
+}
+
+$places_json = json_encode($places, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
+
 close_db_connect($link);
+
 
 include_once '../view/user_page_view.php';
